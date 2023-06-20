75c92e0e62fe12954c35f1a826d1416761b45e2c
yumzh3 <129244098+yumzh3@users.noreply.github.com>
2023-06-20 14:33:58 -0400

    Add files via upload

--- mantle melting_crystallization2023/wl1989models_2023.py
+++ mantle melting_crystallization2023/wl1989models_2023.py
@@ -1,5 +1,11 @@
-###Jocelyn Fuentes 2016 - Based on WL1989
-# Mingzhen Yu 2021 - add Ni and Mn
+# calculate the liquidus temperature to start crystallization
+# temperature decrease by 1 Celsius per step
+# calculate the liquid fraction, phase fractions, liquid and phase compositions in wt.% per each step during crystallization
+# calculte either fractional or equilibrium crystallization
+# detailed algorithm is introduced in Weaver and Langmuir 1990
+# originally written by Jocelyn Fuentes 2016
+# modified by Mingzhen Yu 2021: add Ni and Mn in the system
+# last modified:
 
 from wl1989stoich_2023 import *
 from wl1989kdcalc_2023 import *
@@ -7,17 +13,16 @@ from wlState_2023 import *
 import numpy as np
 import math
 
-
-## from wl1989models2021.py
+# default values used to calculate the phase proportions called by function 'wlState_2023'
 ta = {'cpx':1., 'plg':1., 'ol':2./3.}
 uaj_plg = {'CaAl2O4':5./3., 'NaAlO2':2.5, 'MgO':0., 'FeO':0., 'CaSiO3':0., 'TiO2':0., 'KAlO2':0., 'PO52':0., 'MnO':0.,'NiO':0.} # CaAl2Si2O8, NaAlSi3O8
 uaj_ol = {'CaAl2O4':0., 'NaAlO2':0., 'MgO':1, 'FeO':1, 'CaSiO3':0., 'TiO2':0., 'KAlO2':0., 'PO52':0., 'MnO':1., 'NiO':1.} # (Mg,Fe,Ni,Mn)2SiO4
 uaj_cpx = {'CaAl2O4':4./3., 'NaAlO2':2., 'MgO':2., 'FeO':2., 'CaSiO3':1., 'TiO2':1., 'KAlO2':0., 'PO52':0., 'MnO':2., 'NiO':2.} # CaAl2SiO6, NaAlSi2O6, MgSiO3, FeSiO3, CaSiO3, MnSiO3, NiSiO3, CaTiO3
 uaj = {'ol':uaj_ol, 'plg':uaj_plg, 'cpx':uaj_cpx}
 
-# get the liquidus T, which can be set as T_start
+# calculate the liquidus T (in Kelvin)
 def get_first_T(system_components, P = 1., kdCalc = kdCalc_langmuir1992):
-    firstT = 2000.
+    firstT = 2000.  # a guess for liquidus T
     deltaT = 100.
     qa, fa, major_liquid_components, solid_phase_components, num_iter = state(system_components,firstT,uaj, ta, P=P, kdCalc= kdCalc)
     fl = 1-sum(fa.values()) # liquid fraction in the system
@@ -37,8 +42,7 @@ def get_first_T(system_components, P = 1., kdCalc = kdCalc_langmuir1992):
             firstT = 2000.
     return firstT
 
-# fractional xtalization controlled by temperature, get liquid fraction, phase fractions, liquid and phase compositions in wt.%
-#Fractional Crystallization - System Components change after each iteration
+# calculate fractional xtalization including liquid fraction, phase fractions, liquid and phase compositions in wt.%
 def frac_model_trange(t_start, t_stop, major_start_comp, P=1., kdCalc = kdCalc_langmuir1992):
     tstep = 1.
     #bulk_d = {key:0. for key in trace_start_comp}
@@ -89,7 +93,7 @@ def frac_model_trange(t_start, t_stop, major_start_comp, P=1., kdCalc = kdCalc_l
             #trace_dict[elem].append(trace_liquid_comp[elem])
     return fl, fa_dict, major_oxide_dict, major_phase_oxide_dict #, trace_dict   
 
-#Equilibrium Crystallization - System Components never Change 
+# calculate equilibrium xtalization including liquid fraction, phase fractions, liquid and phase compositions in wt.%
 def eq_model_trange(t_start, t_stop, major_start_comp, P = 1., kdCalc = kdCalc_langmuir1992):
     tstep = 1.
     #bulk_d = {key:0. for key in trace_start_comp}
